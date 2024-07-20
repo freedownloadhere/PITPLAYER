426220a41fa70b6d9ac1455df72a5bb8a8cd130f
GitHub Action <action@github.com>
2024-07-20 16:40:53 +0000

    Initialize template repository proper
    Set package name to com.github.freedownloadhere.pitplayer
    Set project name to PitPlayer
    Set modid to pitplayer

--- src/main/java/com/github/freedownloadhere/pitplayer/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/freedownloadhere/pitplayer/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.freedownloadhere.pitplayer.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
